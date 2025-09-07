9d2677701a7365c889238559360a0b36991f8267
Youssefr1 <youssframa09@gmail.com>
2025-09-07 20:29:33 +0300

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:astro/core/app/env.variable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'astro_store_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
 
   await Firebase.initializeApp();
 
-  runApp(const Beda3tyStoreApp());
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then ((_){
+    runApp(const Beda3tyStoreApp());
+  });
+
+
 }
